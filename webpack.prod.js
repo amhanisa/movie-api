0bb2c409f988cca1f3bf1dee41af54d148d2b491
Azka Muhammad <azka@amhanisa.com>
2020-07-23 14:40:32 +0700

    setting webpack

---
+++ webpack.prod.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const common = require("./webpack.common");
+const {
+    merge
+} = require("webpack-merge")
+const {
+    CleanWebpackPlugin
+} = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+var OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = merge(common, {
+    mode: "production",
+    output: {
+        filename: "[name].bundle.[contentHash].js",
+        path: path.resolve(__dirname, "dist")
+    },
+    optimization: {
+        minimizer: [
+            new OptimizeCssAssetsPlugin(),
+            new TerserPlugin()
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].[contentHash].css"
+        }),
+        new CleanWebpackPlugin(),
+    ],
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                'css-loader',
+                'sass-loader'
+            ],
+
+        }, {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        }]
+    }
+
+});
